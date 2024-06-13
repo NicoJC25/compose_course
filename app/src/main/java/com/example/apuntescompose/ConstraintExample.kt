272653c6c39acc61555a52572c6e1b2af625d5f3
NicoJC25 <nejuez@misena.edu.co>
2024-06-13 09:50:37 -0500

    commit constraints finalizados, finalizado y comentado seccion de textfields, finalizado y comentado seccion de states, finalizado y comentado seccion de state hoisting

--- app/src/main/java/com/example/apuntescompose/ConstraintExample.kt
+++ app/src/main/java/com/example/apuntescompose/ConstraintExample.kt
@@ -12,31 +12,33 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 
+
+//NOTA IMPORTANTE: Recordar hacer antes de cualquier cosa relacionada con los constraint layout, importarlos en el gradle.
 @Composable
-fun ConstraintExample1() {
-    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+fun ConstraintExample1() { //Funcion que contendra el constraint layout
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {//Constraint Layout definido
 
-        val (boxRed, boxBlue, boxYellow, boxMagenta, boxGreen) = createRefs()
+        val (boxRed, boxBlue, boxYellow, boxMagenta, boxGreen) = createRefs() //Se pueden definir varios componentes a la vez si son del mismo tipo
 
-        Box(modifier = Modifier
+        Box(modifier = Modifier //Modificamos la primera caja
             .size(125.dp)
             .background(Color.Red)
-            .constrainAs(boxRed) {
-                top.linkTo(parent.top)
+            .constrainAs(boxRed) {//"constrainsAs" sirve para indicarle a la caja que va a enlazarse con una variabe o componente creado antes
+                top.linkTo(parent.top) //De aqui a abajo, son las uniones de tipo constraint de cada lado a algo que este en el layout
                 start.linkTo(parent.start)
                 end.linkTo(parent.end)
                 bottom.linkTo(parent.bottom)
             })
 
-        Box(modifier = Modifier
+        Box(modifier = Modifier //Modificamos la segunda caja
             .size(125.dp)
             .background(Color.Blue)
             .constrainAs(boxBlue) {
-                top.linkTo(boxRed.bottom)
+                top.linkTo(boxRed.bottom) //Aqui ya se usa conexion de constraint layout con otra caja, en vez que con el parent
                 end.linkTo(boxRed.start)
             })
 
-        Box(modifier = Modifier
+        Box(modifier = Modifier //Modificamos la tercera caja
             .size(125.dp)
             .background(Color.Yellow)
             .constrainAs(boxYellow) {
@@ -44,7 +46,7 @@ fun ConstraintExample1() {
                 end.linkTo(boxRed.start)
             })
 
-        Box(modifier = Modifier
+        Box(modifier = Modifier //Modificamos la cuarta caja
             .size(125.dp)
             .background(Color.Magenta)
             .constrainAs(boxMagenta) {
@@ -52,7 +54,7 @@ fun ConstraintExample1() {
                 start.linkTo(boxRed.end)
             })
 
-        Box(modifier = Modifier
+        Box(modifier = Modifier //Modificamos la quinta caja
             .size(125.dp)
             .background(Color.Green)
             .constrainAs(boxGreen) {
@@ -64,19 +66,19 @@ fun ConstraintExample1() {
 }
 
 @Composable
-fun ConstraintExampleGuie() {
-    ConstraintLayout(Modifier.fillMaxSize()) {
-        // val startGuide = createGuidelineFromTop(16.dp)
+fun ConstraintExampleGuie() { //Funcion para explicar como funcionan las guias
+    ConstraintLayout(Modifier.fillMaxSize()) {//Creamos el constraint layout
+        // val startGuide = createGuidelineFromTop(16.dp) //Una forma de definir una guia
 
-        val boxRed = createRef()
-        val topGuide = createGuidelineFromTop(0.1f)
-        val startGuide = createGuidelineFromStart(0.25f)
+        val boxRed = createRef() //Creamos una caja
+        val topGuide = createGuidelineFromTop(0.1f) //Creamos una guia por la parte de arriba que ocupe siempre el 10% de cualquier dispositivo donde se ejecute la aplicacion
+        val startGuide = createGuidelineFromStart(0.25f) //Creamos otra guia por la parte izquierda que ocupe el 25%
 
-        Box(modifier = Modifier
+        Box(modifier = Modifier //Creamos y modificamos la caja
             .size(125.dp)
             .background(Color.Red)
             .constrainAs(boxRed) {
-                top.linkTo(topGuide)
+                top.linkTo(topGuide) //La conectamos a la guia
                 start.linkTo(startGuide)
             })
     }
@@ -84,19 +86,19 @@ fun ConstraintExampleGuie() {
 
 
 @Composable
-fun ConstraintBarrier() {
-    ConstraintLayout(Modifier.fillMaxSize()) {
-        val (boxRed, boxGreen, boxYellow) = createRefs()
-        val barrier = createEndBarrier(boxRed, boxGreen)
+fun ConstraintBarrier() { //Funcion para explicar como funcionan las barreras
+    ConstraintLayout(Modifier.fillMaxSize()) { //Creamos el constraint layout
+        val (boxRed, boxGreen, boxYellow) = createRefs() //Se crean 3 variables de cajas
+        val barrier = createEndBarrier(boxRed, boxGreen) //Y a estos dos se les va a crear una barrera al lado derecho
 
-        Box(modifier = Modifier
+        Box(modifier = Modifier //Creamos y modificamos una caja
             .size(125.dp)
             .background(Color.Green)
             .constrainAs(boxGreen) {
-                start.linkTo(parent.start, margin = 16.dp)
+                start.linkTo(parent.start, margin = 16.dp) //Se puede agregar margen aqui mismo donde se conecta la caja
             })
 
-        Box(modifier = Modifier
+        Box(modifier = Modifier //Creamos y modificamos otra caja
             .size(225.dp)
             .background(Color.Red)
             .constrainAs(boxRed) {
@@ -104,23 +106,23 @@ fun ConstraintBarrier() {
                 start.linkTo(parent.start, margin = 32.dp)
             })
 
-        Box(
+        Box( //Creamos y modificamos la ultima caja
             Modifier
                 .size(50.dp)
                 .background(Color.Yellow)
                 .constrainAs(boxYellow) {
-                    start.linkTo(barrier)
+                    start.linkTo(barrier) //El lado izquierdo de esta caja estara conectado a la barrera
                 })
-    }
+    } //Como tal, se podria decir que las barreras sirven para evitar que una caja se sobreponga en otra, aunque no se tenga clara del to-do su funcionalidad en casos reales
 }
 
 @Preview
 @Composable
-fun ConstraintChainsExample() {
-    ConstraintLayout(Modifier.fillMaxSize()) {
-        val (boxRed, boxGreen, boxYellow) = createRefs()
+fun ConstraintChainsExample() { //Funcion para explicar las cadenas
+    ConstraintLayout(Modifier.fillMaxSize()) {//Creacion del constraint layout
+        val (boxRed, boxGreen, boxYellow) = createRefs() //Creacion de las variables de cajas
 
-        Box(modifier = Modifier
+        Box(modifier = Modifier //Creacion y modificacion de la primera caja
             .size(125.dp)
             .background(Color.Green)
             .constrainAs(boxGreen) {
@@ -128,7 +130,7 @@ fun ConstraintChainsExample() {
                 end.linkTo(boxRed.start)
             })
 
-        Box(modifier = Modifier
+        Box(modifier = Modifier //Creacion y modificacion de la segunda caja
             .size(125.dp)
             .background(Color.Red)
             .constrainAs(boxRed) {
@@ -136,7 +138,7 @@ fun ConstraintChainsExample() {
                 end.linkTo(boxYellow.start)
             })
 
-        Box(modifier = Modifier
+        Box(modifier = Modifier //Creacion y modificacion de la tercera caja
             .size(125.dp)
             .background(Color.Yellow)
             .constrainAs(boxYellow) {
@@ -145,5 +147,8 @@ fun ConstraintChainsExample() {
             })
 
         createHorizontalChain(boxRed, boxGreen, boxRed, chainStyle = ChainStyle.Spread)
+        //Creacion de una cadena. Una cadena tiene como funcion, conectar varios componentes entre si como un grupo...
+        //...En este caso, es una cadena horizontal por las posiciones de las cajas, se pone cuales seran los componentes...
+        //...A conectar, y el tipo de conexion, ya sea que todas esten unidas y separadas, o unidas sin separacion.
     }
 }
--- app/src/main/java/com/example/apuntescompose/MainActivity.kt
+++ app/src/main/java/com/example/apuntescompose/MainActivity.kt
@@ -20,12 +20,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldColors
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -38,7 +53,12 @@ class MainActivity : ComponentActivity() {
         setContent {
             ApuntesComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyRow(
+
+                    var myText by remember { mutableStateOf("") } //Para entender esta parte, esperar a la seccion 5
+
+                    MyStateHoistingTextField(
+                        name = myText, //Para entender esta parte, esperar a la seccion 5
+                        {myText = it}, //Para entender esta parte, esperar a la seccion 5
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -47,6 +67,8 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
+//Seccion 2: Layouts en Jetpack Compose
 @Composable
 fun MyColumn() {
     //Column (Modifier.fillMaxSize(), verticalArrangement = Arrangement.SpaceBetween) {
@@ -161,7 +183,8 @@ fun MyComplexLayout() { //Funcion que va a utilizar los otros layouts juntos
             Modifier
                 .fillMaxWidth() //Aqui le decimos que ocupe toda la anchura
                 .weight(1f) //Le decimos que va a tener un peso de 1 ya que se va a dividir el peso con otros layouts
-                .background(Color.Magenta), contentAlignment = Alignment.Center) {//Color de fondo magenta y centrar contenido ya que se va a poner un texto
+                .background(Color.Magenta), contentAlignment = Alignment.Center
+        ) {//Color de fondo magenta y centrar contenido ya que se va a poner un texto
             Text(text = "Hola, soy Nicolas Juez") //Texto de la caja
         }
 
@@ -170,17 +193,22 @@ fun MyComplexLayout() { //Funcion que va a utilizar los otros layouts juntos
         Row( //Creamos el segundo layout el cual sera una fila
             Modifier
                 .fillMaxWidth() //Maximo de anchura
-                .weight(1f)) {//Peso de 1
-            Box(modifier = Modifier //Y dentro de la fila, va a ir otra caja
-                .weight(1f) //La cual tendra un peso ya que se hara otra caja despues
-                .fillMaxHeight() //Que llene el maximo de altura
-                .background(Color.Red), contentAlignment = Alignment.Center){//Color a la caja y centrar para poner texto
+                .weight(1f)
+        ) {//Peso de 1
+            Box(
+                modifier = Modifier //Y dentro de la fila, va a ir otra caja
+                    .weight(1f) //La cual tendra un peso ya que se hara otra caja despues
+                    .fillMaxHeight() //Que llene el maximo de altura
+                    .background(Color.Red), contentAlignment = Alignment.Center
+            ) {//Color a la caja y centrar para poner texto
                 Text(text = "Hola, soy Nicolas Juez") //Texto de la caja
             }
-            Box(modifier = Modifier //Segunda caja de la fila
-                .weight(1f) //Peso de 1
-                .fillMaxHeight() //Maximo de altura
-                .background(Color.Blue), contentAlignment = Alignment.Center) {//Color de fondo y centrar contenido
+            Box(
+                modifier = Modifier //Segunda caja de la fila
+                    .weight(1f) //Peso de 1
+                    .fillMaxHeight() //Maximo de altura
+                    .background(Color.Blue), contentAlignment = Alignment.Center
+            ) {//Color de fondo y centrar contenido
                 Text(text = "Hola, soy Nicolas Juez") //Texto de la segunda caja
             }
         }
@@ -188,19 +216,144 @@ fun MyComplexLayout() { //Funcion que va a utilizar los otros layouts juntos
             Modifier
                 .fillMaxWidth() //Maximo de anchura
                 .weight(1f) //Peso de 1
-                .background(Color.Yellow), contentAlignment = Alignment.BottomCenter) {//Color de fondo y centrar contenido para texto
+                .background(Color.Yellow), contentAlignment = Alignment.BottomCenter
+        ) {//Color de fondo y centrar contenido para texto
             Text(text = "Hola, soy Nicolas Juez") //Texto de la caja
         }
     }
 }
 
 @Composable
-fun MySpacer(size:Int){ //Para los spacer, los cuales son espacios pequeños dentro de la apk, se suele hacer una funcion aparte, en esta se establece altura editable
+fun MySpacer(size: Int) { //Para los spacer, los cuales son espacios pequeños dentro de la apk, se suele hacer una funcion aparte, en esta se establece altura editable
     Spacer(modifier = Modifier.height(size.dp)) //Modificador para poder editar la altura
 }
 
-@Preview( showSystemUi = true) //Mostrar preview completa para lo que se este viendo
+//Seccion 3: Estados en Compose
+@Composable
+fun MyStateExample(modifier: Modifier = Modifier) { //Funcion que explica los estados, metodos que se pueden usar para almacenar en memoria cosas
+    // var counter = remember { mutableStateOf(0) } //Primero, se crea un contador que sera un estado mutable, tendrá como caracteristica tambien la palabra remember, para que no olvide la cantidad de veces que ha sido pulsado
+    // val counter = rememberSaveable { mutableStateOf(0) } //rememberSaveable va a permitir que, asi sea cambie de rotacion la pantalla, no se pierdan los datos del contador (ciclo de vida de android)
+    var counter by rememberSaveable { mutableStateOf(0) } //Utilizamos by para usar el state property (getValue, setValue)
+    Column( //Creamos y definimos una columna
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally //Tambien puede tener modificaciones horizontales, aunque no todas
+    ) {
+        // Button(onClick = { counter.value += 1 }) {//Creamos un boton que al clickear, aumente en 1 el valor del contador
+        Button(onClick = { counter += 1 }) {//Retiramos el .value si se esta usando el state property (getValue, setValue)
+            Text(text = "Pulsar") //El texto del boton
+        }
+        Text(text = "He sido pulsado ${counter} veces") //Texto que ira debajo del boton, al ser un estado, se tiene que llamar al valor del estado como tal
+    }
+}
+
+//Seccion 4: Componentes Text y TextField
+@Composable
+fun MyText(
+    modifier: Modifier = Modifier
+) {
+    Column(Modifier.fillMaxSize()) {
+        Text(text = "Esto es un ejemplo xd") //Forma basica de poner un texto
+        Text(text = "Esto es un ejemplo xd", color = Color.Cyan) //Forma de asignar color a un texto
+        Text(
+            text = "Esto es un ejemplo xd",
+            fontWeight = FontWeight.ExtraBold
+        ) //Forma de asignar una decoracion de fuerza del texto, en este caso que tenga mucha negrilla
+        Text(
+            text = "Esto es un ejemplo xd",
+            fontWeight = FontWeight.Light
+        ) //Aqui, que sea algo mas delgado y claro el texto
+        Text(
+            text = "Esto es un ejemplo xd",
+            style = TextStyle(fontFamily = FontFamily.Cursive)
+        ) //Se pueden hacer estilos propios, en este caso, un estilo propio de letra cursiva
+        Text(
+            text = "Esto es un ejemplo xd",
+            textDecoration = TextDecoration.LineThrough
+        ) //Este metodo es para asignar una decoracion como una linea a traves del texto
+        Text(
+            text = "Esto es un ejemplo xd",
+            textDecoration = TextDecoration.Underline
+        ) //Aqui es con una linea por debajo, como subrayado
+        Text(
+            text = "Esto es un ejemplo xd",
+            textDecoration = TextDecoration.combine( //Se pueden combinar varios tipos de decoracion del texto
+                listOf(
+                    TextDecoration.Underline,
+                    TextDecoration.LineThrough
+                ) //Como en este caso, la linea de por medio y la linea de debajo
+            )
+        )
+
+    }
+}
+
+@Composable
+fun MyTextField() { //Funcion de explicacion del TextField, equivalente al EditText de xml
+    var myText by remember { mutableStateOf("") } //Creamos un estado ya que es necesario, para pasarlo como valor del TextField. Recordar importar getValue y setValue si se va a usar el by
+    TextField(
+        value = myText,
+        onValueChange = {
+            myText = it
+        }) //Creamos el TextField, pasamos por valor el estado y que cuando se cambie algo, que ahora el estado sea equivalente al texto que el usuario escribio en el TextField
+}
+
+@Composable
+fun MyTextFieldAdvance( //Funcion para mostrar una version mas avanzada de los TextField
+    modifier: Modifier = Modifier
+) {
+    var myText by remember { mutableStateOf("") } //State
+
+    TextField(value = myText, onValueChange = {//Creamos el TextField y pasamos de valor el estado
+        myText =
+            if (it.contains("a")) { //Y que cuando se escriba algo, si se escribio alguna palabra que tenga la letra "a"
+                it.replace(
+                    "a",
+                    ""
+                ) //Que la letra a se reemplace con un espacio vacio o en resumidas cuentas, que se borre
+            } else { //Si no
+                it //Entonces que se pueda escribir normal esa otra letra
+            }
+    },
+        label = { Text(text = "Introduzca su nombre ") }) //Label sirve como placeholder para indicar que se debe escribir en el textfield
+}
+
+@OptIn(ExperimentalMaterial3Api::class) //Import necesario en esta funcion, ya que el metodo "outlinedTextFieldColors esta deprecado y no deja usarse en modo normal"
+@Composable
+fun MyTextFieldOutlined( //Funcion para explicar este textfield. Lo que hace es que aparece una linea alrededor del textfield, linea que se puede cambiar de color al editar el texto
+    modifier: Modifier = Modifier
+) {
+    var myText by remember { mutableStateOf("") } //State
+
+    OutlinedTextField(
+        value = myText,
+        onValueChange = {
+            myText = it
+        }, //Definicion del outlinedtextfield, practicamente es lo mismo que el textfield normal
+        modifier = Modifier.padding(24.dp), //Padding para que no este pegado arriba ni a la izquieda
+        label = { Text(text = "Escribe lo que quieras") }, //Placeholder
+        colors = TextFieldDefaults.outlinedTextFieldColors( //Se usa este metodo para cambiar los colores de las lineas cuando hay o no texto escrito y recordar que es deprecable el metodo
+            focusedBorderColor = Color.Magenta, //Cambio cuando haya algo escrito
+            unfocusedBorderColor = Color.Blue //Para cuando no haya nada escrito
+        )
+    )
+}
+
+//Seccion 5: State hoisting
+@Composable
+fun MyStateHoistingTextField( //Funcion para hacer state hoising, o, en resumidas cuentas, volver el state en un padre
+    name: String, //El name o el state, se le pasa como parametro, el name estara ubicado en la clase padre
+    onValuedChanged: (String) -> Unit, //Se le pasa la lambda tambien como parametro
+    modifier: Modifier = Modifier
+) {
+    TextField(
+        value = name, //Se pasa como value el name
+        onValueChange = { //Cuando se escriba algo
+            onValuedChanged(it) }) //Se pasa la funcion que se paso antes como parametro
+}
+
+@Preview(showSystemUi = true) //Mostrar preview completa para lo que se este viendo
 @Composable
 fun MyPreview() {
-    MyComplexLayout()
+    MyTextFieldOutlined()
 }
